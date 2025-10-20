f62f9da35ad8a64907bdb949183c3745aa307d82
Archana01011996 <archana.jan01@gmail.com>
2025-10-20 13:55:05 +0530

    Second commit

---
+++ ArchanaProject/src/Javabasics_eclipse/HelloWorld.java
@@ -0,0 +1,12 @@
+package Javabasics_eclipse;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello WOrld");
+		System.out.println("My First java class to push to git hub using eclipse");
+		
+
+	}
+
+}
